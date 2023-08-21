f900b65848ec0c2165ab5e75982bd5842cda269e
anhnx <anhnx@DESKTOP-JOE4G31>
2023-08-21 14:03:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharpBasic
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pass by reference
+            People people1 = new People();
+            people1.Name = "People 1";
+            System.Console.WriteLine("Name before: " + people1.Name);
+            ChangeName(people1);
+            System.Console.WriteLine("Name after: " + people1.Name);
+
+            //Pass by value
+            int num = 10;
+            System.Console.WriteLine("num before: " + num);
+            ChangeValue(num);
+            System.Console.WriteLine("num after: " + num);
+
+        }
+        static void ChangeName(People people)
+        {
+            people.Name = "Test";
+        }
+        static void ChangeValue(int num)
+        {
+            num = 100;
+        }
+    }
+
+    class People
+    {
+        private string? name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
